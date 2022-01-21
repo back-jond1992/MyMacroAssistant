f3299773d6ceecd48a37deb68551aa87fa3e455a
back-jond1992 <b.jond7@outlook.com>
2022-01-21 11:48:32 +0000

    adds styling to home screen

--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -1,5 +1,12 @@
 import React, {useContext} from 'react';
-import {View, Text, StyleSheet, Image, Dimensions} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Dimensions,
+  ScrollView,
+} from 'react-native';
 import userContext from '../../contexts/userContexts/UserContext';
 
 const screenSize = Dimensions.get('screen');
@@ -14,49 +21,69 @@ const HomeScreen = () => {
   }
 
   return (
-    <View style={styles.root}>
-      <Text style={styles.text_primary}>Hi {user.name}</Text>
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={styles.root}>
+        <Text style={styles.text_primary}>Hi {user.name}</Text>
 
-      <Image style={styles.image} source={{uri: user.avatar_url}} />
+        <View style={styles.details_container}>
+          <View style={styles.image_container}>
+            <Image style={styles.image} source={{uri: user.avatar_url}} />
+          </View>
 
-      <Text style={styles.text_secondary}>Weight: {user.weight}lbs</Text>
+          <View style={styles.text_container}>
+            <Text style={styles.text_secondary}>Weight: {user.weight}lbs</Text>
 
-      <Text style={styles.text_secondary}>Height: {user.height}"</Text>
+            <Text style={styles.text_secondary}>Height: {user.height}"</Text>
 
-      <Text style={styles.text_secondary}>Age: {user.age} yeas old.</Text>
+            <Text style={styles.text_secondary}>Age: {user.age} yeas old.</Text>
 
-      <Text style={styles.text_secondary}>
-        Maintenance calories: {user.maintenance}
-      </Text>
+            <Text style={styles.text_secondary}>
+              Maintenance calories: {user.maintenance}
+            </Text>
 
-      <Text style={styles.text_secondary}>Target calories: {user.target}</Text>
-
-      {/* <Image /> */}
-    </View>
+            <Text style={styles.text_secondary}>
+              Target calories: {user.target}
+            </Text>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   root: {
-    alignItems: 'center',
-    padding: 20,
+    padding: 10,
+  },
+  details_container: {
+    flexDirection: 'row',
+  },
+  image_container: {
+    justifyContent: 'flex-start',
+  },
+  text_container: {
+    justifyContent: 'flex-start',
+    marginLeft: 10,
   },
   text_primary: {
     alignItems: 'center',
     padding: 20,
     fontSize: 24,
+    fontWeight: 'bold',
+    color: '#000000',
   },
   text_secondary: {
     alignItems: 'center',
-    padding: 20,
-    fontSize: 12,
+    padding: 2,
+    fontSize: 14,
+    color: '#000000',
   },
   image: {
     borderRadius: screenSize.width * 0.15,
     borderWidth: 1,
     borderColor: 0x000000ff,
-    width: screenSize.width * 0.35,
-    height: screenSize.width * 0.35,
+    width: screenSize.width * 0.3,
+    height: screenSize.width * 0.3,
   },
 });
 
